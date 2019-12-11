f6e530d0f78c94515c2fe8b67dc7710026f22aa7
Douglas Bernardes <douglaspb96@gmail.com>
2019-12-11 19:19:17 -0300

    Finalizada migração

--- src/Rest/ConfigAPI.js
+++ src/Rest/ConfigAPI.js
@@ -4,7 +4,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
-const URL_BASE_SERVER_API = isDevelopment ? "http://"+window.location.hostname+":4040/" : "http://167.172.230.69/api/"; //"https://api.rbernardes.com.br/api";
+const URL_BASE_SERVER_API = isDevelopment ? "http://"+window.location.hostname+":4040/" : "https://api.rbernardes.com.br/";
 const URL_BASE_API = URL_BASE_SERVER_API+"api";
 
 const httpLink = createUploadLink({
