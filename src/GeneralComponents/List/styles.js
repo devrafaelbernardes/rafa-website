cd843817312a71e0c5b4f6ea206eb03b0694788e
Douglas Bernardes <douglaspb96@gmail.com>
2020-04-14 23:47:08 -0300

    Update

--- src/GeneralComponents/List/styles.js
+++ src/GeneralComponents/List/styles.js
@@ -4,6 +4,9 @@ import { Row } from 'react-bootstrap';
 export const Container = styled(Row)``;
 
 export const NotFoundContainer = styled(Row)`
+    justify-content: center;
+    align-items: center;
+    text-align: center;
     padding: 15px;
     font-size: ${({ theme }) => theme.sizes.fonts.normal};
     color: ${({ theme }) => theme.colors.text_light};
