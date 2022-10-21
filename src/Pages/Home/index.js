5fb2d8158aeca9bebe5ae4b9f2204f477305c352
Douglas Bernardes <douglaspb96@gmail.com>
2022-10-21 13:59:35 -0300

    Add new links

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,19 +1,19 @@
-import ListMedia from 'GeneralComponents/ListMedia';
-import React from 'react';
-import { Row } from 'react-bootstrap';
-import { Container, Footer, General } from './styles';
+import ListMedia from "GeneralComponents/ListMedia";
+import React from "react";
+import { Row } from "react-bootstrap";
+import { Container, Footer, General } from "./styles";
 
 export function Home() {
-    return (
-        <Container>
-            <General>
-                <Row>
-                    <ListMedia isLandingPage />
-                </Row>
-                <Footer />
-            </General>
-        </Container>
-    );
+  return (
+    <Container>
+      <General>
+        <Row>
+          <ListMedia isLandingPage />
+        </Row>
+        <Footer />
+      </General>
+    </Container>
+  );
 }
 
 export default Home;
--- src/Pages/Struct/Header/Components/Menu/index.js
+++ src/Pages/Struct/Header/Components/Menu/index.js
@@ -1,50 +1,67 @@
-import List from 'GeneralComponents/List';
-import React from 'react';
-import { AboutURL, HomeURL, MediaURL, SocialNetworkURL } from 'Routers/URLs';
-import { URL_CONTACT } from 'StaticContent/ExternalURLs';
-import Texts from 'StaticContent/Texts';
-import { Container, OptionMenu } from './styles';
+import List from "GeneralComponents/List";
+import React from "react";
+import { AboutURL, HomeURL, MediaURL, SocialNetworkURL } from "Routers/URLs";
+import {
+  URL_BAGS,
+  URL_CONTACT,
+  URL_COURSES,
+  URL_MODELINGS,
+} from "StaticContent/ExternalURLs";
+import Texts from "StaticContent/Texts";
+import { Container, OptionMenu } from "./styles";
 
 export function Menu() {
-    return (
-        <Container>
-            <List
-                items={[
-                    {
-                        to: HomeURL().REDIRECT.BASE,
-                        text: Texts.HOME,
-                    },
-                    {
-                        to: MediaURL().REDIRECT.BASE,
-                        text: Texts.MEDIA,
-                    },
-                    {
-                        to: AboutURL().REDIRECT.BASE,
-                        text: Texts.ABOUT,
-                    },
-                    {
-                        href: URL_CONTACT,
-                        text: Texts.CONTACT,
-                    },
-                    {
-                        to: SocialNetworkURL().REDIRECT.BASE,
-                        text: Texts.SOCIAL_NETWORK,
-                    },
-                ]}
-                renderItem={(item, key) => (
-                    <OptionMenu
-                        key={key}
-                        exact={key === 0}
-                        to={item.to}
-                        href={item.href}
-                        isNavLink={item.to ? true : false}
-                    >
-                        {item.text}
-                    </OptionMenu>
-                )}
-            />
-        </Container>
-    );
+  return (
+    <Container>
+      <List
+        items={[
+          {
+            to: HomeURL().REDIRECT.BASE,
+            text: Texts.HOME,
+          },
+          {
+            href: URL_MODELINGS,
+            text: Texts.MODELINGS,
+          },
+          {
+            href: URL_COURSES,
+            text: Texts.COURSES,
+          },
+          {
+            href: URL_BAGS,
+            text: Texts.BAGS,
+          },
+          {
+            to: MediaURL().REDIRECT.BASE,
+            text: Texts.MEDIA,
+          },
+          {
+            to: AboutURL().REDIRECT.BASE,
+            text: Texts.ABOUT,
+          },
+          {
+            href: URL_CONTACT,
+            text: Texts.CONTACT,
+          },
+          {
+            to: SocialNetworkURL().REDIRECT.BASE,
+            text: Texts.SOCIAL_NETWORK,
+          },
+        ]}
+        renderItem={(item, key) => (
+          <OptionMenu
+            key={key}
+            exact={key === 0}
+            to={item.to}
+            href={item.href}
+            isNavLink={item.to ? true : false}
+          >
+            {item.text}
+          </OptionMenu>
+        )}
+      />
+    </Container>
+  );
 }
 
 export default Menu;
