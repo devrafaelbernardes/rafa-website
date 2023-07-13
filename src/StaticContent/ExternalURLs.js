bca277303ca0f56789ba82dd4c10e03b97ff7c2b
Douglas <devrafaelbernardes@gmail.com>
2023-07-13 09:00:37 -0300

    Change course url

--- src/StaticContent/ExternalURLs.js
+++ src/StaticContent/ExternalURLs.js
@@ -2,8 +2,7 @@ import { isDevelopment } from "./InfoSite";
 
 const STORE = "http://www.lojarbernardes.com.br/";
 export const URL_STORE = STORE;
-export const URL_COURSES =
-  "https://www.lojarbernardes.com.br/cursos-online-ct-2915c3";
+export const URL_COURSES = "https://cursos.rafaelbernardes.com";
 export const URL_BAGS = "https://www.lojarbernardes.com.br/bolsas-ct-311d9f";
 export const URL_MODELINGS =
   "https://www.lojarbernardes.com.br/modelagens-ct-278491";
@@ -11,7 +10,7 @@ export const URL_CONTACT = STORE + "contact.html";
 export const URL_WORKSHOP = STORE + "workshop-ct-257942";
 export const URL_DASHBOARD = isDevelopment
   ? "http://localhost:3010/"
-  : "https://dashboard.rbernardes.com.br/";
+  : "https://dashboard.rafaelbernardes.com/";
 export const URL_EAD = isDevelopment
   ? "http://localhost:3020/"
-  : "https://ead.rbernardes.com.br/";
+  : URL_COURSES;
